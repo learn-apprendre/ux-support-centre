3c9cace52972834d8842f0adbd190cf977141ff2
learn-apprendre <76952542+learn-apprendre@users.noreply.github.com>
2021-01-08 17:25:56 -0500

    Add files via upload

---
+++ assets/popup-onscroll-plugin/script.js
@@ -0,0 +1,2 @@
+
+(function(a){a(document).ready(function(){if(!a("html").hasClass("is-builder")){var b=!0;a(window).scroll(function(){b&&0!==document.documentElement.scrollTop&&80<=(document.documentElement.scrollTop+window.innerHeight)/document.documentElement.offsetHeight*100&&(a(".mbr-popup[data-on-scroll]").each(function(b,c){a(c).modal("show")}),b=!1)})}})})(jQuery);
